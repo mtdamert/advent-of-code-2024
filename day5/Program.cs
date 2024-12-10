f2a53ce3b8fc5c1e9a6910cd35b729006e04fb91
Mike Damert <mtdamert@gmail.com>
2024-12-09 19:59:57 -0500

    Added part 2 of Day 5 code

--- day5/Program.cs
+++ day5/Program.cs
@@ -1,8 +1,23 @@
 ﻿
+static int GetIndex(string[] updateSet, string rule)
+{
+    if (updateSet == null)
+        return -1;
+
+    for (int i = 0; i < updateSet.Length; i++)
+    {
+        if (updateSet[i] == rule)
+            return i;
+    }
+
+    return -1;
+}
+
 string line;
 List<string[]> pageOrderingRules = new List<string[]>();
 List<string[]> updates = new List<string[]>();
 int middlePageSum = 0;
+int invalidMiddlePageSum = 0;
 try
 {
     StreamReader sr = new StreamReader(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\input");
@@ -49,6 +64,7 @@ foreach (string[] updateSet in updates)
     for (int i=0; i<updateSet.Length; i++)
     {
         string update = updateSet[i];
+        reorderLine:
 
         // If this number appears as the second part of any rule, make sure the first part of the rule doesn't appear after it
         foreach (string[] rule in pageOrderingRules)
@@ -69,9 +85,35 @@ foreach (string[] updateSet in updates)
 
     if (isInCorrectOrder)
     {
-        //Console.WriteLine("Line with middle of " + updateSet[updateSet.Length / 2] + " was valid");
         middlePageSum += Convert.ToInt32(updateSet[updateSet.Length / 2]);
+    }
+
+    if (!isInCorrectOrder)
+    {
+        bool orderChanged = false;
+        do
+        {
+            orderChanged = false;
+            for (int ruleNumber = 0; ruleNumber<pageOrderingRules.Count; ruleNumber++)
+            {
+            string[] rule = pageOrderingRules[ruleNumber];
+
+                // If both halves of a rule exist in this update set, make sure they're in the correct order
+                int index0 = GetIndex(updateSet, rule[0]);
+                int index1 = GetIndex(updateSet, rule[1]);
+                if (index0 != -1 && index1 != -1 && index0 > index1)
+                {
+                    string tempUpdate = updateSet[index0];
+                    updateSet[index0] = updateSet[index1];
+                    updateSet[index1] = tempUpdate;
+                    orderChanged = true;
+                }
+            }
+        } while (orderChanged);
+
+        invalidMiddlePageSum += Convert.ToInt32(updateSet[updateSet.Length / 2]);
     }
 }
 
 Console.WriteLine("Sum of middle numbers in valid updates: " + middlePageSum);
+Console.WriteLine("Sum of middle numbers in re-ordered invalid updates: " + invalidMiddlePageSum);
